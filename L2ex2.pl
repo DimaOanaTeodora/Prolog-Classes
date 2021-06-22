aed936db8f054d1d11fa8312d75d603ca58a096c
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-22 21:46:38 +0300

    Update L2ex2.pl

--- L2ex2.pl
+++ L2ex2.pl
@@ -7,17 +7,28 @@ Fn = Fn-2 + Fn-1, pt n>=2
 fib (N, X) = X este al N-lea termen in sirul lui Fibonacci
 */
 
+% --- Varianta 1-----
+
 fib(0, 1).
 fib(1, 1).
-fib(N, X) :- N1 is N-1, N2 is N-2, fib(N2, F1), fib(N1, F2), X is F1+F2.
-/* Gresit pt ca trebuie sa fortez evaluare lui N-1, N-2:
-fib(N, X) :- X is fib(n-2, X) + fib(n-1, X).
-In plus, fib este predicat deci intoarce true si false, is asteapta expresie aritmetica*/
+fib(N, X) :- 
+      N1 is N-1,
+      N2 is N-2,
+      fib(N1, F1), 
+      fib(N2, F2), 
+      X is F1+F2.
 
-/* functie fibonaci care e mai eficienta */
+/* Gresit: fib(N, X) :- X is fib(N-2, X) + fib(N-1, X).
+In plus, fib este predicat deci intoarce true si false,
+is asteapta expresie aritmetica*/
+
+% --- Varianta 2(mai eficienta)-----
 fibonaci(N, X) :- aux(N, _, X).
 
 aux(0, 1, 1).
 aux(1, 1, 1).
-aux(N, F2, Y) :- N1 is N-1, aux(N1, F1, F2), Y is F1+F2.
+aux(N, F2, Y) :- 
+      N1 is N-1,
+      aux(N1, F1, F2),
+      Y is F1+F2.
 
