021a3a599ab1e3d603e1be0278f7fb1888bdecf2
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-22 22:00:27 +0300

    Update L2ex3.pl

--- L2ex3.pl
+++ L2ex3.pl
@@ -7,8 +7,26 @@ trebuie afisat.
 */
 
 % printez N caractere C pe o linie
-/* ai grija la ce e dupa apelul recrusiv pt ca le pune la final 
-daca aveam nl dupa apelul recrusiv imi punea la finalul randului cu N caractere, cele N new line-uri
+/* 
+- ai grija la ce e dupa apelul recrusiv pt ca le pune la final 
+- daca aveam nl dupa apelul recrusiv imi punea la finalul randului cu N caractere,
+ cele N new line-uri
 */
+
+% ---- Deseneaza o linie cu N caractere ----
 line(0, _).
-line(N, C) :- N1 is N-1, write(C), line(N1, C).
+line(N, C) :- 
+      N1 is N-1,
+      write(C),
+      line(N1, C).
+
+% ---- Printeaza de N ori o linie cu N caractere (L trebuie sa fie constant) ----
+matrix(0, _, _).
+matrix(N, L, C) :-
+      N1 is N-1,
+      nl,
+      line(L, C),
+      matrix(N1, L, C). 
+
+square(0, _).
+square(N, C) :- matrix(N, N, C).
